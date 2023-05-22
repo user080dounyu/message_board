e6e144bae2ae7e582005737d9824f8c0b460ff6e
shunsuke.furugen <2023dounyukenshu.080@gmail.com>
2023-05-22 16:48:59 +0900

    seisaku

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
+})
 @Table(name = "messages")
 public class Message {
     @Id
